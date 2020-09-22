c05c5f8402e2bf8a3f237bc1383eef3e9cad9149
ezelopes <up872640@myport.ac.uk>
2020-09-22 18:38:40 +0100

    Getting physiotherapists from the front end patient

--- src/pages/patient/searchPhysiotherapist.jsx
+++ src/pages/patient/searchPhysiotherapist.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../../config/firebase.config';
+
+const functions = firebase.functions();
 
 const ProfilePage = () => {
+
+  const [physiotherapistsList, setPhysiotherapistsList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => { 
+     const response = await getPhysiotherapists(); 
+     setPhysiotherapistsList(response);
+    }
+
+    fetchData();
+  }, []);
+
+  const getPhysiotherapists = async () => {
+    try {
+      const getAllPhysiotherapists = functions.httpsCallable('getAllPhysiotherapists');
+      const response = await getAllPhysiotherapists();
+      console.log(response);
+
+      return response.data.physiotherapists;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div> HERE YOU WILL SEARCH FOR PHYSIOTHERAPISTS </div>
+    <>
+      <h2> SEND REQUEST TO YOUR PREFERRED PHYSIOTHERAPISTS </h2>
+      <br />
+      { physiotherapistsList.map((currentPhysiotherapist, index) => {
+          return <div key={index}>
+            { currentPhysiotherapist.email }
+          </div>
+      }) }
+    </>
   );
 }
 
