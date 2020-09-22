1698a7bf8a1368669774fb28468afe1f2f85dcb9
ezelopes <up872640@myport.ac.uk>
2020-09-22 17:06:31 +0100

    Draft for bones / muscles videos pages

--- src/pages/musclesPage.jsx
+++ src/pages/musclesPage.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+import { Container, Row, Col } from 'react-bootstrap'
 
 const MusclesPage = () => {
+
+  const youtubeVideosID = ['N2ukHIBBFAA', 'T7sBeFEu4pw', '1uwAyZ2RyLQ', 'dFpVHc5vtSY', '_nBlN9yp9R8'];
+  
+  const opts = {
+    height: '170',
+    width: '240',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
   return (
-    <div> Muscles Page </div>
-  );
+    <>
+      <h2> Muscles Page </h2>
+
+      <Container>
+        <Row>
+          { youtubeVideosID.map((currentID) => {
+              return <div id={currentID} key={currentID}>
+                  <Col lg={true} style={{ marginTop: '1.5em' }}>
+                    <YouTube videoId={currentID} opts={opts} />
+                  </Col>
+                </div>
+              })
+          }
+        </Row>
+      </Container>
+    </>
+  );  
 }
 
 export default MusclesPage;
