68938a96e64737c1c6ab15c36674bd4f68110761
ezelopes <up872640@myport.ac.uk>
2020-10-04 12:08:16 +0100

    Responsive Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -38,36 +38,39 @@ const NavBar = () => {
   }, []);
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="/">PhysioAtHome</Navbar.Brand>
-        <Nav className="mr-auto">
-          
-          { currentUserSignedIn && <Nav.Link href="/muscles"> Muscles </Nav.Link> }
-          { currentUserSignedIn && <Nav.Link href="/bones"> Bones </Nav.Link> }
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">
+        
+        { currentUserSignedIn && <Nav.Link href="/muscles"> Muscles </Nav.Link> }
+        { currentUserSignedIn && <Nav.Link href="/bones"> Bones </Nav.Link> }
 
-          { (currentRole === 'ADMIN' && currentUserSignedIn ) && <Nav.Link href="/admin/promoteToAdmin"> Promote Users </Nav.Link> }
-          { (currentRole === 'PHYSIOTHERAPIST' && currentUserSignedIn ) && <Nav.Link href="/physio/personalPatients"> My Patients </Nav.Link> }
-          { (currentRole === 'PHYSIOTHERAPIST' && currentUserSignedIn ) && <Nav.Link href="/physio/patientInvites"> Invites </Nav.Link> }
-          { (currentRole === 'PATIENT' && currentUserSignedIn ) && <Nav.Link href="/patient/addNewSymptomPage"> Add New Symptom </Nav.Link> }
-          { (currentRole === 'PATIENT' && currentUserSignedIn ) && <Nav.Link href="/patient/searchphysiotherapist"> Search Physiotherapists </Nav.Link> }
-         
-        </Nav>
+        { (currentRole === 'ADMIN' && currentUserSignedIn ) && <Nav.Link href="/admin/promoteToAdmin"> Promote Users </Nav.Link> }
+        { (currentRole === 'PHYSIOTHERAPIST' && currentUserSignedIn ) && <Nav.Link href="/physio/personalPatients"> My Patients </Nav.Link> }
+        { (currentRole === 'PHYSIOTHERAPIST' && currentUserSignedIn ) && <Nav.Link href="/physio/patientInvites"> Invites </Nav.Link> }
+        { (currentRole === 'PATIENT' && currentUserSignedIn ) && <Nav.Link href="/patient/addNewSymptomPage"> Add New Symptom </Nav.Link> }
+        { (currentRole === 'PATIENT' && currentUserSignedIn ) && <Nav.Link href="/patient/searchphysiotherapist"> Search Physiotherapists </Nav.Link> }
+        
+      </Nav>
         <Navbar.Text style={{ marginRight: '1em' }}> 
           { currentRole !== 'null' ? currentRole : '' }
         </Navbar.Text>
-      <Form inline>
-        { currentUserSignedIn
-          ? <Button 
-              href='/loginPage'
-              variant="outline-info"
-              onClick={userLogOut}
-            > Log Out </Button>
-          : <Button
-              href='/loginPage'
-              variant="outline-info"
-            > Log In </Button>
-        }
-      </Form>
+        <Form>
+          { currentUserSignedIn
+            ? <Button 
+                href='/loginPage'
+                variant="danger"
+                onClick={userLogOut}
+              > Log Out </Button>
+            : <Button
+                href='/loginPage'
+                variant="success"
+              > Log In / Sign Up </Button>
+          }
+        </Form>
+      </Navbar.Collapse>
     </Navbar>
   )
 };
