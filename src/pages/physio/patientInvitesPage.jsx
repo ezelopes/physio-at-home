7b83eb808d82e63b3f8e3606f9c59bf240a75879
ezelopes <up872640@myport.ac.uk>
2020-10-04 11:13:16 +0100

    Accept and Decline Invite from Physiotherapist Page

--- src/pages/physio/patientInvitesPage.jsx
+++ src/pages/physio/patientInvitesPage.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import firebase from '../../config/firebase.config';
-
+import { ToastContainer, toast } from 'react-toastify';
 import { Button, Card, Container, Row, Col } from 'react-bootstrap'
+
+import firebase from '../../config/firebase.config';
+import toastConfig from '../../config/toast.config';
+import 'react-toastify/dist/ReactToastify.css';
 
 const functions = firebase.functions();
 
@@ -38,22 +41,53 @@ const PatientInvitesPage = () => {
   }
 
   const acceptInviteRequest = async (patientID, name, email, photoURL) => {
-    console.log({ physioID: userInfo.uid, patientID, name, email, photoURL });
-    const physioID =  userInfo.uid;
-    const acceptInviteRequest = functions.httpsCallable('acceptInviteRequest');
-    const response = await acceptInviteRequest({ physioID, patientID, name, email, photoURL });
-    console.log(response);
+    try {
+      console.log({ physioID: userInfo.uid, patientID, name, email, photoURL });
+      const physioID =  userInfo.uid;
+      document.getElementById(`${patientID}-acceptButton`).disabled = true;
+      document.getElementById(`${patientID}-declineButton`).disabled = true;
+      document.getElementById(`${patientID}-acceptButton`).textContent = 'Loading...';
+
+      const acceptInviteRequest = functions.httpsCallable('acceptInviteRequest');
+      const response = await acceptInviteRequest({ physioID, patientID, name, email, photoURL });
+      console.log(response);
+
+      document.getElementById(`${patientID}-acceptButton`).textContent = 'Accepted ✔️';
+      toast.success('🚀 Invite Accepted Successfully!', toastConfig);
+    } catch (err) {
+      toast.error('😔 There was an error accepting the invite!', toastConfig);
+      document.getElementById(`${patientID}-acceptButton`).className = 'btn btn-warning'; // -danger?
+      document.getElementById(`${patientID}-acceptButton`).textContent = 'Refresh Page!';
+      console.log(err);
+    }
   }
   
   const declineInviteRequest = async (patientID) => {
-    const physioID =  userInfo.uid;
-    const declineInviteRequest = functions.httpsCallable('declineInviteRequest');
-    const response = await declineInviteRequest({ physioID, patientID });
-    console.log(response);
+    // ❌
+    try {
+      console.log({ physioID: userInfo.uid, patientID });
+      const physioID =  userInfo.uid;
+      document.getElementById(`${patientID}-acceptButton`).disabled = true;
+      document.getElementById(`${patientID}-declineButton`).disabled = true;
+      document.getElementById(`${patientID}-declineButton`).textContent = 'Loading...';
+
+      const declineInviteRequest = functions.httpsCallable('declineInviteRequest');
+      const response = await declineInviteRequest({ physioID, patientID });
+      console.log(response);
+
+      document.getElementById(`${patientID}-declineButton`).textContent = 'Declined ❌';
+      toast.success('🚀 Invite Declined Successfully!', toastConfig);
+    } catch (err) {
+      toast.error('😔 There was an error declining the invite!', toastConfig);
+      document.getElementById(`${patientID}-declineButton`).className = 'btn btn-warning'; // -danger?
+      document.getElementById(`${patientID}-declineButton`).textContent = 'Refresh Page!';
+      console.log(err);
+    }
   }
 
   return (
     <>
+      <ToastContainer />
       <Container style={{ maxWidth: '100%' }}>
         <Row>
         { Object.keys(patientInvitesList).map((patientID, index) => {
@@ -68,10 +102,15 @@ const PatientInvitesPage = () => {
                       <Card.Text>
                         Email: {email}
                       </Card.Text>
-                      <Button variant="success" onClick={() => acceptInviteRequest(patientID, name, email, photoURL)} style={{ marginRight: '1em' }}>
+                      <Button
+                        id={`${patientID}-acceptButton`}
+                        variant="success"
+                        onClick={() => acceptInviteRequest(patientID, name, email, photoURL)}
+                        style={{ marginRight: '1em' }}
+                      >
                         Accept Invite
                       </Button>
-                      <Button variant="danger" onClick={() => declineInviteRequest(patientID)}>
+                      <Button id={`${patientID}-declineButton`} variant="danger" onClick={() => declineInviteRequest(patientID)}>
                         Decline Invite
                       </Button>
                     </Card.Body>
