42f6ed911a251f39936c8e643391101f044aa98b
ezelopes <up872640@myport.ac.uk>
2020-10-05 23:57:59 +0100

    Removed comments

--- src/components/rangeBar.jsx
+++ src/components/rangeBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 
-const RangeBar = () => {
+const RangeBar = ({ id }) => {
   const inactiveColor = '#EFEFEF'
   const min = 1;
   const max = 300;
@@ -28,6 +28,7 @@ const RangeBar = () => {
     return (
       <div>
         <input
+          id={id}
           ref={inputRef}
           className="inputR"
           name="sliderName"
@@ -38,62 +39,11 @@ const RangeBar = () => {
           onChange={(e) => handleChange(e)}
           style={styleInput}
         />
-        <div className="label" style={{ textAlign: 'center', color: 'white' }}>
+        {/* <div className="label" style={{ textAlign: 'center', color: 'white' }}>
           {rangeBarValue}
-        </div>
+        </div> */}
       </div>
     )
 };
-
-// export default RangeBar;
-
-
-const active = '#64c3ef'
-const inactive = '#dbdbdb'
-
-class RangeComponent extends React.Component {
-  inputRef = React.createRef()
-
-  state = { value: 1 }
-
-  handleChange = (min, max) => (event) => {
-    const value = event.target.value
-    const progress = (value / max) * 100 + '%'
-    this.setState({ value: value })
-    const newBackgroundStyle = `linear-gradient(90deg, ${active} 0% ${progress}%,   ${inactive} ${progress}% 100%)`
-    this.inputRef.current.style.background = newBackgroundStyle
-  }
-
-  render() {
-    const minValue = 1 
-    const maxValue = 300
-    const progress = (this.state.value / maxValue) * 100 + '%'
-
-    const styleInput = {
-
-      background: `linear-gradient(90deg, ${active} 0% ${progress},   ${inactive} ${progress} 100%)`,
-   }
-
-    return (
-      <div>
-        <input
-          ref={this.inputRef}
-          id="sliderId"
-          className="inputR"
-          name="sliderName"
-          type="range"
-          min={minValue}
-          max={maxValue}
-          value={this.state.value}
-          onChange={this.handleChange(minValue, maxValue)}
-          style={styleInput}
-        />
-        <div className="label" style={{textAlign: 'center'}}>
-          {this.state.value}
-        </div>
-      </div>
-    )
-  }
-}
 
 export default RangeBar;
