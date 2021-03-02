8b55f7d8a0dae069f0026655e4be73711587bcf0
ezelopes <up872640@myport.ac.uk>
2021-03-02 23:46:43 +0000

    Exported Firebase Keys to ENV file

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,15 +2,18 @@ import firebase from 'firebase/app';
 import 'firebase/functions';
 import 'firebase/auth';
 
+
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAAro6Z6xYWql0Ova2lOoufw2i78v1rvMU",
-  authDomain: "physioathome-b2cdb.firebaseapp.com",
-  databaseURL: "https://physioathome-b2cdb.firebaseio.com",
-  projectId: "physioathome-b2cdb",
-  storageBucket: "physioathome-b2cdb.appspot.com",
-  messagingSenderId: "561723219960",
-  appId: "1:561723219960:web:bf66bda025ddcde111800d",
-  measurementId: "G-7PNQKRCV73"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
